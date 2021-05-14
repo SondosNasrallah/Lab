2a1db80142f994b792a68e29dc7d577e135533a3
SondosNasrallah <sondos.nasrallah98@gmail.com>
2021-05-14 22:21:03 +0300

    done

--- app.js
+++ app.js
@@ -2,5 +2,19 @@ let userName = prompt('What is your name?');
 console.log(userName);
  
 
+let age =prompt('please enter your age ');
+console.log(age);
+
+let Live=prompt('where do you live ');
+console.log(Live);
+
 let favCol = prompt('What is your favorite color?');
 console.log(favCol);
+
+let like=prompt('Mention something you like about yourself ');
+console.log(like)
+
+
+alert('welcom ' + userName + ' you are live in ' + Live + 
+' thats a wonderfull place actually, you like '
++ like + ' , ' + ' you have ' + age + ' year oh mashallah i wish to you a great life');
